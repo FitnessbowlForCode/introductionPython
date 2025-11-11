6f36341f2c57b98de35b0b5c55252b925d5d51cb
Alexander Gratz <agratz.mmt-b2024@fh-salzburg.ac.at>
2025-11-11 10:14:26 +0100

    Assignment 5

---
+++ exercise5/main.py
@@ -0,0 +1,24 @@
+def next_even_fib_after_N(n):
+    x = 1
+    y = 1
+    result = [1,1]
+
+    for i in range(1,9):
+        temp = y + x
+        result.append(temp)
+        x = temp
+        y = temp - y
+    
+    print(result)
+
+    z = 3
+
+    while(z % 2 != 0):
+
+        z = result[n]    
+        print(z)
+        n = n + 1
+        print(z)
+
+    return print("Das ist das Ergebnis: ", z)
+next_even_fib_after_N(6)
