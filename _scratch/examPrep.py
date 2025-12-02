4b49d6ae0baaf59bb49a79e74ad131931c4c47f2
Alexander Gratz <agratz.mmt-b2024@fh-salzburg.ac.at>
2025-12-02 12:58:25 +0100

    Exam prep backup

---
+++ _scratch/examPrep.py
@@ -0,0 +1,16 @@
+from importFunctions import fString
+import importFunctions as functions
+i = 1
+j = "Hallo"
+while i <= 10:
+    print("Ich bin gerade i", i)
+    i += 1
+else:
+    print("i ist größer/gleich 10")
+
+print(type(i))
+
+fString(j)
+functions.yInteger(2)
+
+print(__name__)
---
+++ _scratch/importFunctions.py
@@ -0,0 +1,6 @@
+def fString (x):
+    y = print(__name__)
+    return(print("My purpose is to represent the variable x: ", x ,"and to print y: ", y))
+
+def yInteger (y):
+    return(print("My passion is to multipli interger y by y: ", y**2))
