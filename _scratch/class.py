a7073e5ab97b4dcd424aeccc8f49f5bbc4f58732
Alexander Gratz <agratz.mmt-b2024@fh-salzburg.ac.at>
2026-01-21 08:47:44 +0100

    Addded class.py

--- _scratch/class.py
+++ _scratch/class.py
@@ -1,9 +1,18 @@
-car = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
-car.update({"price": 15000})
+  def greet(self):
+    print("Function Hello, my name is {} and I'm {} years old".format(self.name,self.age))
 
-print(car)
+  def __str__(self):
+    return "__str__ Hello, my name is {} and I'm {} years old".format(self.name,self.age)
+
+p1 = Person("Emil", 25)
+p2 = Person('Angi', 21)
+
+p1.greet()
+p2.greet()
+
+print(p1)
